878a2de96e9b3af7ea0f8398d1e6b1794ed67d33
bysilva <eepsmr48@hotmail.com>
2024-10-19 14:47:32 -0600

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
-import rehypePrism from '@mapbox/rehype-prism';
-import nextMDX from '@next/mdx';
-import remarkGfm from 'remark-gfm';
+import rehypePrism from '@mapbox/rehype-prism'
+import nextMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -13,7 +13,8 @@ const nextConfig = {
   images: {
     unoptimized: true, // Deshabilitar la optimización de imágenes
   },
-};
+  output: 'export', // Asegúrate de que esta línea esté aquí
+}
 
 const withMDX = nextMDX({
   extension: /\.mdx?$/,
@@ -21,6 +22,6 @@ const withMDX = nextMDX({
     remarkPlugins: [remarkGfm],
     rehypePlugins: [rehypePrism],
   },
-});
+})
 
-export default withMDX(nextConfig);
+export default withMDX(nextConfig)
